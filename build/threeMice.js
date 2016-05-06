f9da632e63a33310f858b83ed56a8b4d1ae4e56e
Martynas Beinoras <martynas.beinoras@gmail.com>
2016-05-06 15:37:58 +0300

    v1.2.0

--- build/threeMice.js
+++ build/threeMice.js
@@ -1,6 +1,6 @@
 /**
  * 
- * @version 1.1.0
+ * @version 1.2.0
  * @author  Martynas Beinoras, https://github.com/martynas-b
  *
  */
@@ -9,7 +9,10 @@ window.ThreeMice = window.ThreeMice || {};
 
 ThreeMice.Events = {
 	MOUSEOVER: "mouseover",
-	MOUSEOUT: "mouseout"
+	MOUSEOUT: "mouseout",
+	MOUSEDOWN: "mousedown",
+	MOUSEUP: "mouseup",
+	MOUSECLICK: "mouseclick"
 };
 
 ThreeMice.Options = {
@@ -20,17 +23,9 @@ ThreeMice.log = function (aMsg) {
 	console.log("ThreeMice: " + aMsg);
 };
 
-ThreeMice.hasMouseOver = function (aObj) {
+ThreeMice.hasEvent = function (aObj, aEvt) {
 	var has = false;
-	if (aObj && (typeof aObj.mouseover === ThreeMice.Options.callbackType)) {
-		has = true;
-	}
-	return has;
-};
-
-ThreeMice.hasMouseOut = function (aObj) {
-	var has = false;
-	if (aObj && (typeof aObj.mouseout === ThreeMice.Options.callbackType)) {
+	if (aObj && (typeof aObj[aEvt] === ThreeMice.Options.callbackType)) {
 		has = true;
 	}
 	return has;
@@ -43,11 +38,13 @@ ThreeMice.Mouse = function (aOpt) {
 	this.size = {width: 0, height: 0};
 	this.renderer = null;
 	this.camera = null;
+	this.controls = null;
+	this.positionObject = null;
 	this.scene = null;
 	this.recursive = false;
-	this.mouse = { position: {x: -1, y: -1}, intersected: null };
+	this.mouse = { position: {x: -1, y: -1}, intersected: null, clicked: null };
 	this.enabled = true;
-	this.enabledEvents = {mouseover: false};
+	this.enabledEvents = {mousemove: false, mousedown: false};
 		
 	function initOptions (aOpt) {
 		if (aOpt) {
@@ -70,36 +67,62 @@ ThreeMice.Mouse = function (aOpt) {
 			if (aOpt.recursive) {
 				self.recursive = aOpt.recursive;
 			}
+			if (aOpt.controls) {
+				self.controls = aOpt.controls;
+			}
 		}
 	}
-		
-	function setMousePosition (event) {		
-		self.mouse.position.x = ((event.clientX - self.offset.left + document.body.scrollLeft) / self.size.width) * 2 - 1;
-		self.mouse.position.y = - ((event.clientY - self.offset.top + document.body.scrollTop) / self.size.height) * 2 + 1;
+	
+	this.resetSize = function () {
+		if (self.container) {
+			self.offset = self.container.getBoundingClientRect();
+		}
+		if (self.renderer) {
+			self.size = self.renderer.getSize();
+		}
+	};
+	
+	function triggerMouseOver (aObj) {
+		if (ThreeMice.hasEvent(aObj, ThreeMice.Events.MOUSEOVER)) {
+			aObj.mouseover(aObj);
+		}
+		self.mouse.intersected = aObj;
 	}
 	
 	function triggerMouseOut () {
 		if (self.mouse.intersected) {
-			if (ThreeMice.hasMouseOut(self.mouse.intersected)) {
+			if (ThreeMice.hasEvent(self.mouse.intersected, ThreeMice.Events.MOUSEOUT)) {
 				self.mouse.intersected.mouseout(self.mouse.intersected);					
 			}
 		}
 		self.mouse.intersected = null;
 	}
 	
-	function triggerMouseOver (aObj) {
-		if (ThreeMice.hasMouseOver(aObj)) {
-			aObj.mouseover(aObj);
+	function triggerMouseDown (aObj) {
+		if (ThreeMice.hasEvent(aObj, ThreeMice.Events.MOUSEDOWN)) {
+			aObj.mousedown(aObj);
 		}
-		self.mouse.intersected = aObj;
+		self.mouse.clicked = aObj;
+	}
+	
+	function triggerMouseUp (aObj) {
+		if (self.mouse.clicked && (self.mouse.clicked === aObj)) {
+			if (ThreeMice.hasEvent(aObj, ThreeMice.Events.MOUSEUP)) {
+				aObj.mouseup(aObj);
+			}
+			if (ThreeMice.hasEvent(aObj, ThreeMice.Events.MOUSECLICK)) {
+				aObj.mouseclick(aObj);
+			}
+		}
+		self.mouse.clicked = null;
 	}
 	
 	function onMouseMove (event) {
-		if (self.enabled && self.enabledEvents.mouseover) {
+		if (self.enabled && self.enabledEvents.mousemove) {
 			setMousePosition(event);
 			var object = getIntersect();
 			if (object) {
-				if (ThreeMice.hasMouseOver(object) || ThreeMice.hasMouseOut(object)) {
+				if (ThreeMice.hasEvent(object, ThreeMice.Events.MOUSEOVER) || ThreeMice.hasEvent(object, ThreeMice.Events.MOUSEOUT)) {
 					if (object !== self.mouse.intersected) {
 						triggerMouseOut();
 						triggerMouseOver(object);
@@ -113,9 +136,46 @@ ThreeMice.Mouse = function (aOpt) {
 	}
 	
 	function onMouseOut (event) {
-		if (self.enabled && self.enabledEvents.mouseover) {
+		if (self.enabled && self.enabledEvents.mousemove) {
 			triggerMouseOut();
 		}
+	}
+	
+	function onMouseDown (event) {
+		if (self.enabled && self.enabledEvents.mousedown) {
+			setMousePosition(event);
+			var object = getIntersect();
+			triggerMouseDown(object);
+		}
+	}
+	
+	function onMouseUp (event) {
+		if (self.enabled && self.enabledEvents.mousedown) {
+			setMousePosition(event);
+			var object = getIntersect();
+			triggerMouseUp(object);
+		}
+	}
+	
+	function setMousePosition (event) {
+		var x = 0;
+		var y = 0;
+		
+		if (event.pageX) {
+			x = event.pageX;
+			y = event.pageY;
+		}
+		else if (event.touches) {
+			x = event.touches[0].pageX;
+			y = event.touches[0].pageY;
+		}
+		else if (event.screenX) {
+			x = event.screenX;
+			y = event.screenY;
+		}		
+		
+		self.mouse.position.x = ((x - self.offset.left + document.body.scrollLeft) / self.size.width) * 2 - 1;
+		self.mouse.position.y = - ((y - self.offset.top + document.body.scrollTop) / self.size.height) * 2 + 1;
 	}
 	
 	function getIntersect () {
@@ -124,9 +184,9 @@ ThreeMice.Mouse = function (aOpt) {
 		var vector = new THREE.Vector3( self.mouse.position.x, self.mouse.position.y, 1 );
 				
 		vector.unproject( self.camera );
-		
-		var ray = new THREE.Raycaster( self.camera.position, vector.sub( self.camera.position ).normalize() );
-		
+						
+		var ray = new THREE.Raycaster( self.positionObject.position, vector.sub( self.positionObject.position ).normalize() );
+						
 		var intersects = ray.intersectObjects( self.scene.children, self.recursive );
 		
 		if ( intersects.length > 0 ) {
@@ -140,6 +200,11 @@ ThreeMice.Mouse = function (aOpt) {
 		if (self.container) {
 			self.container.addEventListener( 'mousemove', onMouseMove, false );
 			self.container.addEventListener( 'mouseout', onMouseOut, false );
+			self.container.addEventListener( 'mousedown', onMouseDown, false );
+			self.container.addEventListener( 'mouseup', onMouseUp, false );
+			
+			self.container.addEventListener( 'touchstart', onMouseDown, false );				
+			self.container.addEventListener( 'touchend', onMouseUp, false );
 		}
 	}
 	
@@ -147,7 +212,10 @@ ThreeMice.Mouse = function (aOpt) {
 		aMesh.on = function (aEvt, aCallback) {
 			aMesh[aEvt] = aCallback;
 			if ((aEvt === ThreeMice.Events.MOUSEOVER) || (aEvt === ThreeMice.Events.MOUSEOUT)) {
-				self.enabledEvents.mouseover = true;
+				self.enabledEvents.mousemove = true;
+			}
+			else if ((aEvt === ThreeMice.Events.MOUSEDOWN) || (aEvt === ThreeMice.Events.MOUSEUP) || (aEvt === ThreeMice.Events.MOUSECLICK)) {
+				self.enabledEvents.mousedown = true;
 			}
 		};
 		return aMesh;
@@ -157,6 +225,12 @@ ThreeMice.Mouse = function (aOpt) {
 		initOptions(aOpt);
 						
 		if (self.container && self.camera && self.scene) {
+
+			self.positionObject = self.camera;
+			if (self.controls) {
+				self.positionObject = self.controls.getObject();
+			}
+			
 			initMouseEvents();
 		}
 		else {
